4c7d47279ffa7b56c58553125e8b4ed854253dc0
吴华 <wuhua@sinobest.cn>
2021-05-25 15:07:15 +0800

    perf: 细节优化

--- src/views/Backup/index.vue
+++ src/views/Backup/index.vue
@@ -52,7 +52,7 @@ export default {
         let date = new Date(value)
         let year = date.getFullYear()
         let month = date.getMonth() + 1
-        let day = date.getDay()
+        let day = date.getDate()
         let hour = date.getHours()
         let minutes = date.getMinutes()
         let second = date.getSeconds()
