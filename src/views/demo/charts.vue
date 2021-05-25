a23b9d9bb5aa94994b9077ec39490680158683ad
吴华 <wuhua@sinobest.cn>
2021-05-25 18:27:14 +0800

    地图描点数据

--- src/views/demo/charts.vue
+++ src/views/demo/charts.vue
@@ -1,92 +1,108 @@
 <template>
-  <!-- <div class="charts">
-    <div class="chart" ref="chart-1"></div>
-    <div class="chart" ref="chart-2" style="margin-top: 20px"></div>
-  </div> -->
   <div>
-    <span>{{count}}</span>
-    <button @click="add"> +1 </button>
+    <div class="mapImg" @mousedown="mousedown">
+      <img src="./map.png" alt="地图">
+      <div class="cover"></div>
+    </div>
+    <el-button @click="clearPoints">清空标记点</el-button>
+    <el-button @click="toggle">转化</el-button>
+    <div>{{pointsList}}</div>
+    <div class="map">
+      <div :style="{'transform': `scale(${scale})`}">
+        <svg height="600" width="600" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <polygon class="svgcontain shunde" style="fill: #5f9726;"  :points="chencun"></polygon>
+          <text x="130" dy="160" style="fill: white">陈村</text>
+          <polygon class="svgcontain beijiao" style="fill: #0190a6;" :points="beijiao"></polygon>
+          <text x="160" dy="270" style="fill: white">北滘</text>
+          <polygon class="svgcontain lundun" style="fill: #bc8534;" :points="lundun"></polygon>
+          <text x="200" dy="370" style="fill: white">伦敦</text>
+          <polygon class="svgcontain lundun" style="fill: #01a5ee;" :points="daliang"></polygon>
+          <text x="300" dy="500" style="fill: white">大良</text>
+        </svg>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
-// import * as echarts from 'echarts';
-export default {
-    setup() {
-        const count = ref(false)
-
-        const add = () => count.value = !count.value
-
-        return { count, add }
+import { defineComponent } from 'vue'
+import chencun from './chencun'
+import beijiao from './beijiao'
+import lundun from './lundun'
+import daliang from './daliang'
+export default defineComponent({
+  data() {
+    return {
+      chencun: '',
+      beijiao: '',
+      lundun: '',
+      daliang: '',
+      scale: 1,
+      pointsList: []
     }
-}
-// export default defineComponent({
-//   name: 'charts',
-//   data() {
-//     return {
+  },
+  methods: {
+    clearPoints() {
+      this.pointsList = []
+    },
+    mousedown(e) {
+      this.pointsList.push([e.clientX, e.clientY])
+      let div = document.createElement('div')
+      div.className = 'pointMarker'
+      div.style.left = `${e.clientX}px`
+      div.style.top = `${e.clientY}px`
+      document.body.appendChild(div)
+    },
+    mousewheel(e) {
+      e = e || window.event;
+      e.preventDefault && e.preventDefault(); //禁止滚动
+      let event = e.wheelDelta || e.detail; // 浏览器兼容
+      if (event > 0) {
+        // 放大
+        this.toMax()
+      }
+      if (event < 0) {
+        // 缩小
+        this.toMin()
+      }
+    },
+    toMax() {
+      if (this.scale < 1.5) {
+        this.scale += 0.1
+      }
+    },
+    toMin() {
+      if (this.scale > 0.5) {
+        this.scale -= 0.1
+      }
+    },
+    toggle() {
+      this.chencun = chencun.map(item => {
+        let x = item[0] - 500
+        let y = item[1]
+        return `${x},${y} `
+      })
       
-//     }
-//   },
-//   mounted() {
-//     this.setCharts_1()
-//   },
-//   methods: {
-//     setCharts_1() {
-//       let dom = this.$refs['chart-1'];
-//       var myChart = echarts.init(dom);
-//       myChart.setOption({
-//         title: {
-//             text: '温度监控(单位:℃)'
-//         },
-//         tooltip: {
-//             trigger: 'axis',
-//             formatter: (params) => {
-//               let res = params[0]
-//               return `
-//                 <span>${res.marker}</span>
-//                 <span>${res.name}</span>
-//                 <span>${res.value}</span>
-//                 ℃
-//               `
-//             }
-//         },
-//         xAxis: {
-//             type: 'category',
-//             data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-//         },
-//         yAxis: {
-//             type: 'value'
-//         },
-//         series: [{
-//             data: [21, 19, 22, 23, 18, 20, 19],
-//             type: 'line'
-//         }],
-//       });
-//     },
-//     setCharts_2() {
-//       let dom = this.$refs['chart-1'];
-//       var myChart = echarts.init(dom);
-//       myChart.setOption({
-//         title: {
-//             text: '温度记录(单位:℃)'
-//         },
-//         tooltip: {},
-//         xAxis: {
-//             type: 'category',
-//             data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-//         },
-//         yAxis: {
-//             type: 'value'
-//         },
-//         series: [{
-//             data: [15, 13, 20, 22, 16, 15, 14],
-//             type: 'line'
-//         }]
-//       });
-//     }
-//   }
-// });
+      this.beijiao = beijiao.map(item => {
+        let x = item[0] - 500
+        let y = item[1]
+        return `${x},${y} `
+      })
+
+      this.lundun = lundun.map(item => {
+        let x = item[0] - 500
+        let y = item[1]
+        return `${x},${y} `
+      })
+
+      this.daliang = daliang.map(item => {
+        let x = item[0] - 500
+        let y = item[1]
+        return `${x},${y} `
+      })
+    }
+  }
+})
 </script>
 
 
@@ -94,5 +110,49 @@ export default {
 .chart {
   width: 600px;
   height: 400px;
+}
+.svgcontain {
+  stroke: #fff; 
+  stroke-width: 3; 
+  cursor:pointer;
+  stroke-linecap: round;
+  opacity: .8;
+}
+.svgcontain:hover {
+  opacity: 1;
+}
+.map {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgb(13, 107, 119);
+}
+.mapImg{
+  position: relative;
+  overflow: hidden;
+  font-size: 0;
+}
+.mapImg img {
+  max-width: 100%;
+  max-height: 100%;
+}
+.cover {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 9;
+}
+</style>
+<style>
+.pointMarker {
+  width: 4px;
+  height: 4px;
+  border-radius: 4px;
+  background: red;
+  position: fixed;
+  z-index: 99;
+  transform: translateX(-2px) translateY(-2px);
 }
 </style>
